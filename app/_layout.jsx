97d18d02952e50e9b72f1d0cc2e325bf6d7d170a
Eniola <opakunleeniolail2019@gmail.com>
2024-07-29 23:20:31 +0100

    feat: set up the project

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import React from 'react'
+
+function _layout() {
+  return (
+    <View>
+        <Text>Hello Layout</Text>
+    </View>
+  )
+}
+
+export default _layout
---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function index() {
+  return (<View>
+    <Text>Hello index.jsx</Text>
+  </View>
+  )
+}
+
+export default index
